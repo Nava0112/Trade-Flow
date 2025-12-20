01822c04f6df5a4b47683833f26dbc80cca13610
Nava0112 <nsmnavarasan@gmail.com>
2025-12-20 17:04:33 +0530

    Incomming change

--- backend/src/routes/wallet.routes.js
+++ backend/src/routes/wallet.routes.js
@@ -8,12 +8,6 @@ import { isSelfOrAdmin, isTransactionOwnerOrAdmin } from '../middleware/auth.mid
 
 export const router = express.Router();
 
-<<<<<<< HEAD
-router.post('/deposit/:id', isSelfRoute, createDepositController);
-router.get('/balance/:id', isSelfRoute, getUserWalletBalanceController);
-router.post('/deposit/confirm/:id', isTransactionOwner, confirmDepositController);
-=======
 router.post('/deposit/:id', isSelfOrAdmin, createDepositController);
 router.get('/balance/:id', isSelfOrAdmin, getUserWalletBalanceController);
 router.post('/deposit/confirm/:transactionId', isTransactionOwnerOrAdmin, confirmDepositController);
->>>>>>> 22b0992 (Auth middleware and routes changed from jwt on every middleware to specified middlewares)
