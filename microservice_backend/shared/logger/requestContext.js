c45e363c696af291e0f8f1d9353d964ef1dafc2e
Nava0112 <nsmnavarasan@gmail.com>
2025-12-28 16:23:37 +0530

    Microservice build started and logger implemented

---
+++ microservice_backend/shared/logger/requestContext.js
@@ -0,0 +1,11 @@
+import { randomUUID } from "crypto";
+
+export const requestContext = (req, res, next) => {
+  const requestId =
+    req.headers["x-request-id"] || randomUUID();
+
+  req.requestId = requestId;
+  res.setHeader("x-request-id", requestId);
+
+  next();
+};
